9ad9db180f2e7eac6aedfebd193fcc12d0952884
whitewarlockmtj <white_warlock_mtj@hotmail.com>
2025-04-10 20:46:41 -0500

    Versión inicial con Docker, tests y CI/CD

--- index.js
+++ index.js
@@ -5,10 +5,8 @@ const PORT = process.env.PORT || 3000;
 app.get('/', (req, res) => res.send('¡Hola Mundo desde Docker!'));
 app.get('/health', (req, res) => res.send({ status: 'ok' }));
 
-app.listen(PORT, () => console.log(`App corriendo en puerto ${PORT}`));
-
-module.exports = app;
-
 if (require.main === module) {
     app.listen(PORT, () => console.log(`App corriendo en puerto ${PORT}`));
 }
+module.exports = app;
+
