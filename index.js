60ea05e7aa496f93d8b244400324e7906df6c55b
whitewarlockmtj <white_warlock_mtj@hotmail.com>
2025-04-15 20:59:21 -0500

    Versión inicial con Docker, tests y CI/CD

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const PORT = process.env.PORT || 3000;
 app.get('/', (req, res) => res.send('¡Hola Mundo desde Docker!'));
 app.get('/health', (req, res) => res.send({ status: 'ok' }));
 
+// Provocar fallo en ESLint por `console.log()`
+console.log('Este es un log que no debería estar en producción');
+
 if (require.main === module) {
     app.listen(PORT, () => console.log(`App corriendo en puerto ${PORT}`));
 }
