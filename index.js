ae6bc9db4c075f36d838973148830e86734add14
whitewarlockmtj <white_warlock_mtj@hotmail.com>
2025-04-15 21:10:12 -0500

    Versión inicial con Docker, tests y CI/CD

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
-import express from 'express';  // Error si estás usando 'require' y 'import'
+const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.get('/', (req, res) => res.send('¡Hola Mundo desde Docker! 1'));
+app.get('/', (req, res) => res.send('¡Hola Mundo desde Docker!'));
 app.get('/health', (req, res) => res.send({ status: 'ok' }));
-
-console.log('Este es un log que no debería estar en producción'); // Error por `console.log()`
-
-const unusedVariable = 'Esta variable nunca se usa'; // Error por variable no utilizada
 
 if (require.main === module) {
     app.listen(PORT, () => console.log(`App corriendo en puerto ${PORT}`));
 }
-module.exports = app;  // Puede generar un error si estás usando ES Modules y 'module.exports'
+
+module.exports = app;
+//// Para ejecutar el servidor, usa el siguiente comando:
+
