f09c290cdcecb69d4418c0cc671bea9ac77b63fb
whitewarlockmtj <white_warlock_mtj@hotmail.com>
2025-04-15 21:06:19 -0500

    Versión inicial con Docker, tests y CI/CD

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-const express = require('express');
-const app = express();
-const PORT = process.env.PORT || 3000;
+const express = require('express')
+const app = express()
+const PORT = process.env.PORT || 3000
+
 
 app.get('/', (req, res) => res.send('¡Hola Mundo desde Docker! 1'));
 app.get('/health', (req, res) => res.send({ status: 'ok' }));
