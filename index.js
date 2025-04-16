b00db7847109209434b0f7cffb9ca15f5c455cc9
whitewarlockmtj <white_warlock_mtj@hotmail.com>
2025-04-15 21:03:03 -0500

    Versión inicial con Docker, tests y CI/CD

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.get('/', (req, res) => res.send('¡Hola Mundo desde Docker!'));
+app.get('/', (req, res) => res.send('¡Hola Mundo desde Docker! 1'));
 app.get('/health', (req, res) => res.send({ status: 'ok' }));
 
 // Provocar fallo en ESLint por `console.log()`
@@ -11,7 +11,4 @@ console.log('Este es un log que no debería estar en producción');
 if (require.main === module) {
     app.listen(PORT, () => console.log(`App corriendo en puerto ${PORT}`));
 }
-
 module.exports = app;
-//// Para ejecutar el servidor, usa el siguiente comando:
-
